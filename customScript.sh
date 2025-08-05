5a397245bdc77b2c362e7ed64e116c903bcba8e9
AdarshDogra-CloudDevops <adarshdogra1122@gmail.com>
2025-08-05 20:12:49 +0530

    Add files via upload

---
+++ customScript.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install -y apache2
+sudo systemctl enable apache2
+sudo systemctl start apache2
+echo "<html><body><h1>Welcome to your Azure VM!</h1></body></html>" | sudo tee /var/www/html/index.html
+sudo systemctl restart apache2
