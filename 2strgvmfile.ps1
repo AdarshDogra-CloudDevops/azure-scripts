79724ba60abd4047b95ac502c5672ca5942fb4d9
AdarshDogra-CloudDevops <adarshdogra1122@gmail.com>
2025-08-14 15:36:06 +0530

    Add files via upload

--- 2strgvmfile.ps1
+++ 2strgvmfile.ps1
@@ -102,7 +102,7 @@ $action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-NoProfil
 write-host "Action set to run PowerShell script: $secondaryScriptPath"
 
 # Register as User account
-Register-ScheduledTask -TaskName $taskName -Trigger $triggerStartup -Action $action -RunLevel Highest -User $adminUsername -Password $adminPassword = ConvertTo-SecureString $secureAdminPassword -AsPlainText -Force
+Register-ScheduledTask -TaskName $taskName -Trigger $triggerStartup -Action $action -RunLevel Highest -User $adminUsername -Password Intern@12345 -Force
 Write-Host "✅ Task Scheduler job created. Secondary script will run after 2 minutes and save files in C:\Users\Public\Downloads"
 
 # Stop transcript for the main script
